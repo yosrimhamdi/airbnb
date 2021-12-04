6538a313df4aad1313042270846ef0fc88a69275
yosrimhamdi <yosrimhamdi@outlook.com>
2021-12-04 22:13:28 +0100

    Added Image entity and its relation to the Ad entity.

--- src/Entity/Ad.php
+++ src/Entity/Ad.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use App\Repository\AdRepository;
 use Cocur\Slugify\Slugify;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -52,6 +54,16 @@ class Ad {
    * @ORM\Column(type="integer")
    */
   private $rooms;
+
+  /**
+   * @ORM\OneToMany(targetEntity=Image::class, mappedBy="ad", orphanRemoval=true)
+   */
+  private $images;
+
+  public function __construct()
+  {
+      $this->images = new ArrayCollection();
+  }
 
   /**
    * @ORM\PrePersist
@@ -134,5 +146,35 @@ class Ad {
     $this->rooms = $rooms;
 
     return $this;
+  }
+
+  /**
+   * @return Collection|Image[]
+   */
+  public function getImages(): Collection
+  {
+      return $this->images;
+  }
+
+  public function addImage(Image $image): self
+  {
+      if (!$this->images->contains($image)) {
+          $this->images[] = $image;
+          $image->setAd($this);
+      }
+
+      return $this;
+  }
+
+  public function removeImage(Image $image): self
+  {
+      if ($this->images->removeElement($image)) {
+          // set the owning side to null (unless already changed)
+          if ($image->getAd() === $this) {
+              $image->setAd(null);
+          }
+      }
+
+      return $this;
   }
 }
