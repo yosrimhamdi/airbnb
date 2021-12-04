9ac5921022475cd876458d879966df813c840e16
yosrimhamdi <yosrimhamdi@outlook.com>
2021-12-04 21:24:03 +0100

    Using slugify package.

--- src/DataFixtures/AdFixtures.php
+++ src/DataFixtures/AdFixtures.php
@@ -2,22 +2,23 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Persistence\ObjectManager;
 use App\Entity\Ad;
+use Cocur\Slugify\Slugify;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AdFixtures extends Fixture {
   public function load(ObjectManager $manager): void {
     $faker = \Faker\Factory::create();
+    $slugify = new Slugify();
 
     for ($i = 0; $i < 10; $i++) {
       $title = str_replace('.', '', $faker->sentence(3));
-      $slug = strtolower(str_replace(' ', '-', $title));
 
       $ad = new Ad();
       $ad
         ->setTitle($title)
-        ->setSlug($slug)
+        ->setSlug($slugify->slugify($title))
         ->setPrice(mt_rand(300, 1400))
         ->setIntroduction($faker->sentence(7))
         ->setContent($faker->paragraph(3))
