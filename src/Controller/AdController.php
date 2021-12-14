42ebd4402059f636728f5a6f2b525a403fd27dbf
yosrimhamdi <yosrimhamdi@outlook.com>
2021-12-14 21:30:04 +0100

    The correct ad image widgets ids.

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -2,14 +2,14 @@
 
 namespace App\Controller;
 
-use App\Repository\AdRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Ad;
 use App\Form\AdType;
+use App\Repository\AdRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdController extends AbstractController {
   /**
