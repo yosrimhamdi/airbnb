97096d8bf18f117807ce6489c535334653869fb6
yosrimhamdi <yosrimhamdi@outlook.com>
2021-12-14 20:45:38 +0100

    Changed an input type.

--- src/Form/ImageType.php
+++ src/Form/ImageType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Image;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,7 +17,7 @@ class ImageType extends AbstractType {
     array $options
   ): void {
     $builder
-      ->add('url', TextType::class, $this->getConfig('Image Url', 'Url'))
+      ->add('url', UrlType::class, $this->getConfig('Image Url', 'Url'))
       ->add(
         'caption',
         TextType::class,
