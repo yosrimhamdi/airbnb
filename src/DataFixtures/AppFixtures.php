3adc5ff26acd354b0997ff8691a0dfb7b7f4db66
yosrimhamdi <yosrimhamdi@outlook.com>
2021-12-15 23:37:32 +0100

    Allowing 0 ads per some users.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,59 +1,48 @@
 <?php
-
 namespace App\DataFixtures;
-
 use App\Entity\Ad;
 use App\Entity\User;
 use App\Entity\Image;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-
 class AppFixtures extends Fixture {
-  public function load(ObjectManager $manager): void {
-    $faker = \Faker\Factory::create();
-    $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
-
-    for ($i = 0; $i < 5; $i++) {
-      $description = "<p>" . join("<p></p>", $faker->paragraphs()) . "</p>";
-
-      $user = new User();
-      $user
-        ->setFirstName($faker->firstNameMale())
-        ->setLastName($faker->lastName())
-        ->setEmail($faker->email())
-        ->setIntroduction($faker->paragraph())
-        ->setDescription($description)
-        ->setHash("1234");
-
-      $manager->persist($user);
-
-      for ($j = 1; $j <= mt_rand(3, 15); $j++) {
-        $content = "<p>" . join("<p></p>", $faker->paragraphs(5)) . "</p>";
-
-        $ad = new Ad();
-        $ad
-          ->setTitle($faker->sentence(3))
-          ->setPrice(mt_rand(300, 1400))
-          ->setIntroduction($faker->sentence(7))
-          ->setContent($content)
-          ->setcoverImage($faker->imageUrl(1980, 700, true))
-          ->setRooms(mt_rand(2, 5))
-          ->setUser($user);
-
-        $manager->persist($ad);
-
-        for ($k = 0; $k < mt_rand(3, 5); $k++) {
-          $image = new Image();
-          $image
-            ->setUrl($faker->imageUrl(1980, 700, true))
-            ->setCaption($faker->sentence(5))
-            ->setAd($ad);
-
-          $manager->persist($image);
+    public function load(ObjectManager $manager): void {
+        $faker = \Faker\Factory::create();
+        $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
+        for ($i = 0; $i < 5; $i++) {
+            $description =
+                "<p>" . join("<p></p>", $faker->paragraphs()) . "</p>";
+            $user = new User();
+            $user
+                ->setFirstName($faker->firstNameMale())
+                ->setLastName($faker->lastName())
+                ->setEmail($faker->email())
+                ->setIntroduction($faker->paragraph())
+                ->setDescription($description)
+                ->setHash("1234");
+            $manager->persist($user);
+            for ($j = 1; $j <= mt_rand(0, 15); $j++) {
+                $content =
+                    "<p>" . join("<p></p>", $faker->paragraphs(5)) . "</p>";
+                $ad = new Ad();
+                $ad->setTitle($faker->sentence(3))
+                    ->setPrice(mt_rand(300, 1400))
+                    ->setIntroduction($faker->sentence(7))
+                    ->setContent($content)
+                    ->setcoverImage($faker->imageUrl(1980, 700, true))
+                    ->setRooms(mt_rand(2, 5))
+                    ->setUser($user);
+                $manager->persist($ad);
+                for ($k = 0; $k < mt_rand(3, 5); $k++) {
+                    $image = new Image();
+                    $image
+                        ->setUrl($faker->imageUrl(1980, 700, true))
+                        ->setCaption($faker->sentence(5))
+                        ->setAd($ad);
+                    $manager->persist($image);
+                }
+            }
         }
-      }
+        $manager->flush();
     }
-
-    $manager->flush();
-  }
 }
