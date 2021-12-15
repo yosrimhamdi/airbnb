bd8739b38f9202d740f4cf59bfe878912e3d588e
yosrimhamdi <yosrimhamdi@outlook.com>
2021-12-15 23:43:26 +0100

    Random user photos.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,43 +6,54 @@ use App\Entity\Image;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 class AppFixtures extends Fixture {
-    public function load(ObjectManager $manager): void {
-        $faker = \Faker\Factory::create();
-        $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
-        for ($i = 0; $i < 5; $i++) {
-            $description =
-                "<p>" . join("<p></p>", $faker->paragraphs()) . "</p>";
-            $user = new User();
-            $user
-                ->setFirstName($faker->firstNameMale())
-                ->setLastName($faker->lastName())
-                ->setEmail($faker->email())
-                ->setIntroduction($faker->paragraph())
-                ->setDescription($description)
-                ->setHash("1234");
-            $manager->persist($user);
-            for ($j = 1; $j <= mt_rand(0, 15); $j++) {
-                $content =
-                    "<p>" . join("<p></p>", $faker->paragraphs(5)) . "</p>";
-                $ad = new Ad();
-                $ad->setTitle($faker->sentence(3))
-                    ->setPrice(mt_rand(300, 1400))
-                    ->setIntroduction($faker->sentence(7))
-                    ->setContent($content)
-                    ->setcoverImage($faker->imageUrl(1980, 700, true))
-                    ->setRooms(mt_rand(2, 5))
-                    ->setUser($user);
-                $manager->persist($ad);
-                for ($k = 0; $k < mt_rand(3, 5); $k++) {
-                    $image = new Image();
-                    $image
-                        ->setUrl($faker->imageUrl(1980, 700, true))
-                        ->setCaption($faker->sentence(5))
-                        ->setAd($ad);
-                    $manager->persist($image);
-                }
-            }
+  public function load(ObjectManager $manager): void {
+    $faker = \Faker\Factory::create();
+    $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
+
+    for ($i = 0; $i < 5; $i++) {
+      $description = "<p>" . join("<p></p>", $faker->paragraphs()) . "</p>";
+
+      $photo =
+        "https://randomuser.me/api/portraits/men/" . mt_rand(1, 99) . ".jpg";
+      $user = new User();
+      $user
+        ->setFirstName($faker->firstNameMale())
+        ->setLastName($faker->lastName())
+        ->setEmail($faker->email())
+        ->setIntroduction($faker->paragraph())
+        ->setDescription($description)
+        ->setPhoto($photo)
+        ->setHash("1234");
+
+      $manager->persist($user);
+
+      for ($j = 1; $j <= mt_rand(0, 15); $j++) {
+        $content = "<p>" . join("<p></p>", $faker->paragraphs(5)) . "</p>";
+
+        $ad = new Ad();
+        $ad
+          ->setTitle($faker->sentence(3))
+          ->setPrice(mt_rand(300, 1400))
+          ->setIntroduction($faker->sentence(7))
+          ->setContent($content)
+          ->setcoverImage($faker->imageUrl(1980, 700, true))
+          ->setRooms(mt_rand(2, 5))
+          ->setUser($user);
+
+        $manager->persist($ad);
+
+        for ($k = 0; $k < mt_rand(3, 5); $k++) {
+          $image = new Image();
+          $image
+            ->setUrl($faker->imageUrl(1980, 700, true))
+            ->setCaption($faker->sentence(5))
+            ->setAd($ad);
+
+          $manager->persist($image);
         }
-        $manager->flush();
+      }
     }
+
+    $manager->flush();
+  }
 }
