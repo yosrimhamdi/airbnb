7b18f13ab727369ff133daa04f10db7d3fa1a08c
yosrimhamdi <yosrimhamdi@outlook.com>
2021-12-17 18:30:11 +0100

    Implemented the login functionality.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Security\Core\User\UserInterface;
+use App\Repository\UserRepository;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
