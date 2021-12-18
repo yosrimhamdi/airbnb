c9f4419e3b830f5933f85d2c08b7c6d0b635cc15
yosrimhamdi <yosrimhamdi@outlook.com>
2021-12-18 22:15:23 +0100

    Displaying login error messages.

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -5,13 +5,20 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AuthController extends AbstractController {
   /**
    * @Route("/login", name="auth_login")
    */
-  public function login(): Response {
-    return $this->render("auth/login.html.twig");
+  public function login(AuthenticationUtils $util): Response {
+    $error = $util->getLastAuthenticationError();
+    $username = $util->getLastUsername();
+
+    return $this->render("auth/login.html.twig", [
+      "error" => $error,
+      "username" => $username,
+    ]);
   }
 
   /**
