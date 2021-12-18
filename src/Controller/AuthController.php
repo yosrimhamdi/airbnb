695846cd1c9e2968952082e431974d378e881b42
yosrimhamdi <yosrimhamdi@outlook.com>
2021-12-18 23:06:19 +0100

    Encrypting the user password while regestring.

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -4,10 +4,12 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AuthController extends AbstractController {
@@ -35,14 +37,23 @@ class AuthController extends AbstractController {
    *
    * @Route("/register", name="auth_register")
    */
-  public function register(Request $request) {
+  public function register(
+    Request $request,
+    EntityManagerInterface $manager,
+    UserPasswordEncoderInterface $enocder
+  ) {
     $user = new User();
 
     $form = $this->createForm(RegistrationType::class, $user);
     $form->handleRequest($request);
 
     if ($form->isSubmitted() && $form->isValid()) {
-      //
+      $user->setPassword($enocder->encodePassword($user, $user->getPassword()));
+
+      $manager->persist($user);
+      $manager->flush();
+
+      return $this->redirectToRoute("auth_login");
     }
 
     return $this->render("auth/register.html.twig", [
