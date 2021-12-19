28ecc5673f82f33023a8c3f54fb2e3102d2eefb0
yosrimhamdi <yosrimhamdi@outlook.com>
2021-12-19 23:26:27 +0100

    Rendering users profiles.

---
+++ src/Controller/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController {
+  /**
+   * @Route("/user/{slug}", name="user_show")
+   */
+  public function show(User $user): Response {
+    return $this->render("user/show.html.twig", [
+      "user" => $user,
+    ]);
+  }
+}
