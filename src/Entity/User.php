e8fd496d176a72240bc0607b15d6394c708c488f
yosrimhamdi <yosrimhamdi@outlook.com>
2021-12-20 13:10:15 +0100

    Generating role table and linking it the users tabl.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -73,8 +73,14 @@ class User implements UserInterface {
    */
   private $ads;
 
+  /**
+   * @ORM\ManyToMany(targetEntity=Role::class, mappedBy="users")
+   */
+  private $userRoles;
+
   public function __construct() {
     $this->ads = new ArrayCollection();
+    $this->userRoles = new ArrayCollection();
   }
 
   public function getId(): ?int {
@@ -213,5 +219,32 @@ class User implements UserInterface {
 
   public function getFullName() {
     return "{$this->firstName} {$this->lastName}";
+  }
+
+  /**
+   * @return Collection|Role[]
+   */
+  public function getUserRoles(): Collection
+  {
+      return $this->userRoles;
+  }
+
+  public function addUserRole(Role $userRole): self
+  {
+      if (!$this->userRoles->contains($userRole)) {
+          $this->userRoles[] = $userRole;
+          $userRole->addUser($this);
+      }
+
+      return $this;
+  }
+
+  public function removeUserRole(Role $userRole): self
+  {
+      if ($this->userRoles->removeElement($userRole)) {
+          $userRole->removeUser($this);
+      }
+
+      return $this;
   }
 }
