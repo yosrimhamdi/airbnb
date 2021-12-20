64ad66568ac45c3f17600b15d047d977e79167e5
yosrimhamdi <yosrimhamdi@outlook.com>
2021-12-20 21:14:32 +0100

    Binding the db role to the default role.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -204,7 +204,15 @@ class User implements UserInterface {
   }
 
   public function getRoles() {
-    return ["ROLE_USER"];
+    $roles = $this->userRoles
+      ->map(function ($role) {
+        return $role->getTitle();
+      })
+      ->toArray();
+
+    $roles[] = "ROLE_USER";
+
+    return $roles;
   }
 
   public function getUsername() {
