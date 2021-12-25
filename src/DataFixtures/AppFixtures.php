4e6fd35d2aa5fbed7b9f30e2ff39ad3d6a37b774
yosrimhamdi <yosrimhamdi@outlook.com>
2021-12-25 12:15:13 +0100

    Updated the admin image in the AppFixtures.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,103 +10,111 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture {
-  private $encoder;
+    private $encoder;
 
-  public function __construct(UserPasswordEncoderInterface $encoder) {
-    $this->encoder = $encoder;
-  }
-
-  public function load(ObjectManager $manager): void {
-    $faker = \Faker\Factory::create();
-    $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
-
-    $adminRole = new Role();
-    $adminRole->setTitle("ROLE_ADMIN");
-
-    $manager->persist($adminRole);
-
-    $adminUser = new User();
-    $adminUser
-      ->setFirstName("Yosri")
-      ->setLastName("Mhamdi")
-      ->setEmail("yosri@mhamdi.co")
-      ->setPassword($this->encoder->encodePassword($adminUser, "password"))
-      ->setIntroduction($faker->paragraph())
-      ->setDescription("<p>" . join("<p></p>", $faker->paragraphs()) . "</p>")
-      ->addUserRole($adminRole)
-      ->setPhoto(
-        "https://scontent.ftun5-1.fna.fbcdn.net/v/t39.30808-6/223663076_4085488201569410_3514209925264326844_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=7mCqatC3pH8AX-HD6SY&tn=TlbynOGAs03aQ3h4&_nc_ht=scontent.ftun5-1.fna&oh=00_AT-MpWH23VUKWYOimzsz0LHqs9_pHG6t_rl5iMicFQicgg&oe=61C41530"
-      );
-
-    $manager->persist($adminUser);
-
-    $users = [$adminUser];
-
-    for ($i = 0; $i < 5; $i++) {
-      $description = "<p>" . join("<p></p>", $faker->paragraphs()) . "</p>";
-
-      $photo =
-        "https://randomuser.me/api/portraits/men/" . mt_rand(1, 99) . ".jpg";
-      $user = new User();
-      $user
-        ->setFirstName($faker->firstNameMale())
-        ->setLastName($faker->lastName())
-        ->setEmail($faker->email())
-        ->setIntroduction($faker->paragraph())
-        ->setDescription($description)
-        ->setPhoto($photo)
-        ->setPassword($this->encoder->encodePassword($user, "password"));
-
-      $users[] = $user;
-
-      $manager->persist($user);
+    public function __construct(UserPasswordEncoderInterface $encoder) {
+        $this->encoder = $encoder;
     }
 
-    for ($j = 1; $j <= mt_rand(0, 15); $j++) {
-      $content = "<p>" . join("<p></p>", $faker->paragraphs(5)) . "</p>";
+    public function load(ObjectManager $manager): void {
+        $faker = \Faker\Factory::create();
+        $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
 
-      $ad = new Ad();
-      $ad
-        ->setTitle($faker->sentence(3))
-        ->setPrice(mt_rand(300, 1400))
-        ->setIntroduction($faker->sentence(7))
-        ->setContent($content)
-        ->setcoverImage($faker->imageUrl(1980, 700, true))
-        ->setRooms(mt_rand(2, 5))
-        ->setUser($users[mt_rand(0, count($users) - 1)]);
+        $adminRole = new Role();
+        $adminRole->setTitle('ROLE_ADMIN');
 
-      $manager->persist($ad);
+        $manager->persist($adminRole);
 
-      for ($k = 0; $k < mt_rand(3, 5); $k++) {
-        $image = new Image();
-        $image
-          ->setUrl($faker->imageUrl(1980, 700, true))
-          ->setCaption($faker->sentence(5))
-          ->setAd($ad);
+        $adminUser = new User();
+        $adminUser
+            ->setFirstName('Yosri')
+            ->setLastName('Mhamdi')
+            ->setEmail('yosri@mhamdi.co')
+            ->setPassword(
+                $this->encoder->encodePassword($adminUser, 'password')
+            )
+            ->setIntroduction($faker->paragraph())
+            ->setDescription(
+                '<p>' . join('<p></p>', $faker->paragraphs()) . '</p>'
+            )
+            ->addUserRole($adminRole)
+            ->setPhoto(
+                'https://www.mhamdi.co/assets/images/pro-pic-of-me.jfif'
+            );
 
-        $manager->persist($image);
-      }
+        $manager->persist($adminUser);
 
-      for ($p = 0; $p < mt_rand(0, 5); $p++) {
-        $createdAt = $faker->dateTimeBetween("-3 months");
-        $startDate = $faker->dateTimeBetween($createdAt);
-        $nbrDays = mt_rand(2, 7);
-        $endDate = (clone $startDate)->modify("+$nbrDays days");
+        $users = [$adminUser];
 
-        $booking = new Booking();
-        $booking
-          ->setAd($ad)
-          ->setBooker($users[mt_rand(0, count($users) - 1)])
-          ->setStartDate($startDate)
-          ->setEndDate($endDate)
-          ->setCreatedAt($createdAt)
-          ->setAmount($ad->getPrice() * mt_rand(1, 7))
-          ->setComment($faker->paragraph());
+        for ($i = 0; $i < 5; $i++) {
+            $description =
+                '<p>' . join('<p></p>', $faker->paragraphs()) . '</p>';
 
-        $manager->persist($booking);
-      }
+            $photo =
+                'https://randomuser.me/api/portraits/men/' .
+                mt_rand(1, 99) .
+                '.jpg';
+            $user = new User();
+            $user
+                ->setFirstName($faker->firstNameMale())
+                ->setLastName($faker->lastName())
+                ->setEmail($faker->email())
+                ->setIntroduction($faker->paragraph())
+                ->setDescription($description)
+                ->setPhoto($photo)
+                ->setPassword(
+                    $this->encoder->encodePassword($user, 'password')
+                );
+
+            $users[] = $user;
+
+            $manager->persist($user);
+        }
+
+        for ($j = 1; $j <= mt_rand(0, 15); $j++) {
+            $content = '<p>' . join('<p></p>', $faker->paragraphs(5)) . '</p>';
+
+            $ad = new Ad();
+            $ad->setTitle($faker->sentence(3))
+                ->setPrice(mt_rand(300, 1400))
+                ->setIntroduction($faker->sentence(7))
+                ->setContent($content)
+                ->setcoverImage($faker->imageUrl(1980, 700, true))
+                ->setRooms(mt_rand(2, 5))
+                ->setUser($users[mt_rand(0, count($users) - 1)]);
+
+            $manager->persist($ad);
+
+            for ($k = 0; $k < mt_rand(3, 5); $k++) {
+                $image = new Image();
+                $image
+                    ->setUrl($faker->imageUrl(1980, 700, true))
+                    ->setCaption($faker->sentence(5))
+                    ->setAd($ad);
+
+                $manager->persist($image);
+            }
+
+            for ($p = 0; $p < mt_rand(0, 5); $p++) {
+                $createdAt = $faker->dateTimeBetween('-3 months');
+                $startDate = $faker->dateTimeBetween($createdAt);
+                $nbrDays = mt_rand(2, 7);
+                $endDate = (clone $startDate)->modify("+$nbrDays days");
+
+                $booking = new Booking();
+                $booking
+                    ->setAd($ad)
+                    ->setBooker($users[mt_rand(0, count($users) - 1)])
+                    ->setStartDate($startDate)
+                    ->setEndDate($endDate)
+                    ->setCreatedAt($createdAt)
+                    ->setAmount($ad->getPrice() * mt_rand(1, 7))
+                    ->setComment($faker->paragraph());
+
+                $manager->persist($booking);
+            }
+        }
+
+        $manager->flush();
     }
-
-    $manager->flush();
-  }
 }
