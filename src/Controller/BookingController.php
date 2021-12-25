55c9cb70b503d7180b67cc78eb3944487d9a19a3
yosrimhamdi <yosrimhamdi@outlook.com>
2021-12-25 07:49:16 +0100

    Showing the book details after booking.

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -32,11 +33,28 @@ class BookingController extends AbstractController {
 
             $manager->persist($booking);
             $manager->flush();
+
+            return redirectToRoute('booking_show', ['id' => $booking->getId()]);
         }
 
         return $this->render('booking/book.html.twig', [
             'form' => $form->createView(),
             'ad' => $ad,
+        ]);
+    }
+
+    /**
+     * Showing a single booking based on its Id
+     *
+     * @Route("/booking/{id}", name="booking_show")
+     * @Security("is_granted('ROLE_USER') and user === booking.getBooker() ")
+     *
+     * @param Booking $booking
+     * @return Response
+     */
+    public function show(Booking $booking) {
+        return $this->render('booking/show.html.twig', [
+            'booking' => $booking,
         ]);
     }
 }
