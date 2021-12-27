d7fa56a16b618a2bf03b9e43ceec781a3c38760c
yosrimhamdi <yosrimhamdi@outlook.com>
2021-12-27 19:41:05 +0100

    Booking date validation.

--- src/Entity/Booking.php
+++ src/Entity/Booking.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\BookingRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=BookingRepository::class)
@@ -19,11 +20,13 @@ class Booking {
 
     /**
      * @ORM\Column(type="datetime")
+     * @Assert\Date()
      */
     private $startDate;
 
     /**
      * @ORM\Column(type="datetime")
+     * @Assert\Date()
      */
     private $endDate;
 
