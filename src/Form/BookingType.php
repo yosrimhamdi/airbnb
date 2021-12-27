e604da7e468ef9db4ed947d7f23bc0b809fcfa51
yosrimhamdi <yosrimhamdi@outlook.com>
2021-12-27 21:13:34 +0100

    Using bootstrap datepicker.

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -9,35 +9,36 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class BookingType extends ApplicationType {
-  public function buildForm(
-    FormBuilderInterface $builder,
-    array $options
-  ): void {
-    $builder
-      ->add(
-        "startDate",
-        DateType::class,
-        $this->getConfig("Start date", "your first night")
-      )
-      ->add(
-        "endDate",
-        DateType::class,
-        $this->getConfig("End date", "your last night")
-      )
-      ->add(
-        "comment",
-        TextareaType::class,
-        $this->getConfig("Comment", "Write  a comment", [
-          "required" => false,
-        ])
-      );
-  }
+    public function buildForm(
+        FormBuilderInterface $builder,
+        array $options
+    ): void {
+        $builder
+            ->add(
+                'startDate',
+                TextType::class,
+                $this->getConfig('Start date', 'your first night')
+            )
+            ->add(
+                'endDate',
+                TextType::class,
+                $this->getConfig('End date', 'your last night')
+            )
+            ->add(
+                'comment',
+                TextareaType::class,
+                $this->getConfig('Comment', 'Write  a comment', [
+                    'required' => false,
+                ])
+            );
+    }
 
-  public function configureOptions(OptionsResolver $resolver): void {
-    $resolver->setDefaults([
-      "data_class" => Booking::class,
-    ]);
-  }
+    public function configureOptions(OptionsResolver $resolver): void {
+        $resolver->setDefaults([
+            'data_class' => Booking::class,
+        ]);
+    }
 }
